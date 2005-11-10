a4e2712bd8429a8beef37c05b01e8ee8664990be
David Anderson <davea@ssl.berkeley.edu>
2005-11-10 06:06:11 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=8839

---
+++ doc/stats_sites.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("docutil.php");
+
+include("stats_sites.dat");
+
+page_head("Web sites for BOINC statistics");
+echo "
+<p>
+The following web sites show statistics for one or more BOINC projects:
+";
+site_list($stats_sites);
+echo "
+These sites use XML-format data exported by BOINC projects.
+The format is described
+<a href=http://boinc.berkeley.edu/stats.php>here</a>.
+If you are interested in running your own site or
+participating in the development efforts,
+please contact the people listed above.
+
+<h2>Statistics signature images</h2>
+<p>
+The following sites offer dynamically-generated
+images showing your statistics in BOINC projects.
+Use these in your email or message-board signature.
+";
+site_list($sig_sites);
+
+page_tail();
+
+?>
