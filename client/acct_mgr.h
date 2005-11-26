5593eb49def8bb4ef384bda1614dca64f6e3e263
David Anderson <davea@ssl.berkeley.edu>
2005-11-26 00:12:44 +0000

    account manager changes
    svn path=/trunk/boinc/; revision=8936

---
+++ client/acct_mgr.h
@@ -0,0 +1,79 @@
+// Berkeley Open Infrastructure for Network Computing
+// http://boinc.berkeley.edu
+// Copyright (C) 2005 University of California
+//
+// This is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation;
+// either version 2.1 of the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// To view the GNU Lesser General Public License visit
+// http://www.gnu.org/copyleft/lesser.html
+// or write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef _ACCT_MGR_
+#define _ACCT_MGR_
+
+#include <string>
+#include <vector>
+
+#include "miofile.h"
+#include "gui_http.h"
+
+// represents info stored on files
+//
+struct ACCT_MGR_INFO {
+	// the following used to be std::string but there
+	// were mysterious bugs where setting it to "" didn't work
+	//
+    char acct_mgr_name[256];
+    char acct_mgr_url[256];
+    char login_name[256];
+    char password[256];
+    double next_rpc_time;
+
+    ACCT_MGR_INFO();
+    int parse_url(MIOFILE&);
+    int parse_login(MIOFILE&);
+    int write_info();
+    int init();
+    void clear();
+    bool poll();
+};
+
+// stuff after here related to RPCs to account managers
+
+struct AM_ACCOUNT {
+    std::string url;
+    std::string authenticator;
+    bool suspend;
+
+    int parse(MIOFILE&);
+    AM_ACCOUNT() {}
+    ~AM_ACCOUNT() {}
+};
+
+struct ACCT_MGR_OP: public GUI_HTTP_OP {
+    int error_num;
+    ACCT_MGR_INFO ami;
+        // a temporary copy while doing RPC.
+        // CLIENT_STATE::acct_mgr_info is authoratative
+    std::string error_str;
+    std::vector<AM_ACCOUNT> accounts;
+    int run_mode;
+    double repeat_sec;
+    int do_rpc(std::string url, std::string name, std::string password);
+    int parse(MIOFILE&);
+    virtual void handle_reply(int http_op_retval);
+
+    ACCT_MGR_OP(){}
+    virtual ~ACCT_MGR_OP(){}
+};
+
+#endif
---
+++ doc/acct_mgt.php
@@ -0,0 +1,152 @@
+<?php
+require_once("docutil.php");
+
+page_head("Account management systems");
+
+echo "
+<p>
+To create an account with BOINC projects, a participant must:
+<ul>
+<li> Locate BOINC project web sites, e.g. using Google.
+<li> Read the web sites, and decide which to join;
+<li> Download and install the BOINC client software.
+</ul>
+and then for each selected project:
+<ul>
+<li> Go through the Attach to Project wizard.
+</ul>
+
+<p>
+If the participant chooses N projects,
+there are N web sites to visit and N Wizards to complete.
+This is tedious if there are lots of projects.
+
+<p>
+This document describes BOINC's support for <b>account management systems</b>,
+which streamline the process of finding and joining BOINC projects.
+A typical account management system is implemented as a web site.
+The participant experience is:
+<ul>
+<li> Visit the account manager site,
+set up a 'meta-account' (name, email, password),
+browse a list of projects, and click checkboxes to select projects.
+<li> Download and install the BOINC client software from the account manager.
+<li> Enter the meta-account name and password in a BOINC client dialog.
+</ul>
+This requires many fewer interactions than the manual approach.
+
+<h2>Implementation</h2>
+<p>
+An account management system works as follows:
+<br>
+<img src=acct_mgt2.png>
+<br>
+
+<ol>
+<li> The participant sets up his meta-account and selects projects.
+<li> The account manager issues a <b>create account</b> RPC
+to each selected project.
+<li> 
+the participant downloads and installs the BOINC client software
+from the account manager.
+The install package includes a file
+(specific to the account manager)
+containing the URL of the account manager.
+<li> The BOINC client runs, and asks the participant to enter
+the name and password of his meta-account.
+<li> The BOINC client does a <b>query accounts</b> RPC
+to the account manager, obtaining a list of accounts.
+It then attaches to these accounts and proceeds.
+</ol>
+
+The <b>create account</b> RPCs
+are described <a href=web_rpc.php>here</a>.
+
+<h2>Core client functionality</h2>
+<p>
+The BOINC core client uses the following files to
+keep track of account manager information.
+<dl>
+<dt>
+<b>acct_mgr_url.xml</b>
+<dd>
+This file identifies the account manager.
+It is typically bundled with the BOINC client in
+an installer package.
+Its format is:
+".html_text("
+<acct_mgr>
+    <name>Name of BOINC account management system</name>
+    <url>http://acctmgr.com/</url>
+</acct_mgr>
+")."
+
+<p>
+Note: the URL is that of the account manager's web site.
+
+<dt>
+<b>acct_mgr_login.xml</b>
+<dd>
+This file contains meta-account information.
+Its format is:
+".html_text("
+<acct_mgr_login>
+   <login>name</login>
+   <password>xxx</password>
+</acct_mgr_login>
+")."
+</dl>
+If the core client finds acct_mgr_url.xml but not acct_mgr_login.xml,
+it prompts for a name and password,
+stores them in acct_mgr_login.xml,
+and makes an account manager RPC.
+The core client offers menu items for making an account manager RPC,
+and for changing the name/password.
+
+<h2>Account manager RPCs</h2>
+
+";
+
+list_start();
+list_item("URL", "<b>BASE_URL/rpc.php</b>, where BASE_URL is the URL
+        of the account manager web site.");
+list_item("input", "name
+    <br>password
+    <br> [ host_cpid ]
+    "
+);
+list_item("output",
+    html_text("<acct_mgr_reply>
+    <name>Account Manager Name</name>
+    [ <error>MSG</error> ]
+    [ <run_mode>X</run_mode> always/auto/never]
+    [ <repeat_sec>xxx</repeat_sec> ]
+    [ 
+      <account>
+         <url>URL</url>
+         <authenticator>KEY</authenticator>
+         [ suspend ]
+      </account>
+        ...
+    ]
+</acct_mgr_reply>")
+);
+list_item("action",
+    "Returns a list of the accounts associated with this meta-account.
+    The 'host_cpid' argment identifies the host.
+    To make it comparable with the host CPID in stats files,
+    do MD5(host_cpid+email_addr).
+    <p>
+    Optionally returns a command to set the run mode (always, auto, or never),
+    commands to suspend individual projects,
+    and a time interval after which another RPC should be done.
+    <p>
+    NOTE: the XML must be as above, with the &lt;url>
+    and &lt;authenticator> elements on a single line,
+    and the &lt;account> and &lt;/account> tags
+    on separate lines."
+);
+list_end();
+
+page_tail();
+?>
