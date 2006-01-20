2d06c1ca83f534062e0a7ec2287cda6917f5c074
Charlie Fenton <charlief@example.com>
2006-01-20 14:04:06 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=9275

---
+++ clientgui/mac/SetVersion.C
@@ -0,0 +1,224 @@
+/*
+ *  SetVersion.c
+ *  boinc
+ *
+ *  Created by Charlie Fenton on 3/29/05.
+ *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+// Set STAND_ALONE TRUE if testing as a separate applicaiton
+#define STAND_ALONE 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "version.h"
+
+#if STAND_ALONE
+#include <unistd.h>     // for getcwd if debugging
+#endif
+
+int IsFileCurrent(char* filePath);
+int FixInfoPlistFile(char* myPath);
+int FixInfoPlist_Strings(void);
+int MakeInstallerInfoPlistFile(void);
+
+int main(int argc, char** argv) {
+    int retval = 0, err;
+
+#if STAND_ALONE
+    char myPath[1024];
+    getcwd(myPath, sizeof(myPath));
+    printf("%s\n", myPath);       // For debugging
+    err = chdir("../");
+    getcwd(myPath, sizeof(myPath));
+    printf("%s\n", myPath);       // For debugging
+#endif
+
+    err = FixInfoPlist_Strings();
+    if (err) retval = err;
+    err = FixInfoPlistFile("./Info.plist");
+    if (err) retval = err;
+    err = FixInfoPlistFile("./GR_Info.plist");
+    if (err) retval = err;
+    err = FixInfoPlistFile("./Installer-Info.plist");
+    if (err) retval = err;
+    err = FixInfoPlistFile("./ScreenSaver-Info.plist");
+    if (err) retval = err;
+    err = FixInfoPlistFile("./SystemMenu-Info.plist");
+    if (err) retval = err;
+    err = MakeInstallerInfoPlistFile();
+    return retval;
+}
+
+
+int IsFileCurrent(char* filePath) {
+    FILE *f;
+    char *c, buf[1024];
+    
+    f = fopen(filePath, "r");
+    if (f == 0)
+        return false;
+    for (;;) {
+        c = fgets(buf, sizeof(buf), f);
+        if (c == NULL)
+            break;   // EOF reached without finding correct version string
+        c = strstr(buf, BOINC_VERSION_STRING);
+        if (c) {
+            fclose(f);
+            return true;  // File contains current version string
+        }
+    }
+    fclose(f);
+    return false;  // File does not contain current version string
+}
+
+
+int FixInfoPlist_Strings() {
+    int retval = 0;
+    FILE *f;
+    char *myPath = "./English.lproj/InfoPlist.strings";
+    
+    if (IsFileCurrent(myPath))
+        return 0;
+
+    f = fopen(myPath, "w");
+    if (f)
+    {
+        fprintf(f, "/* Localized versions of Info.plist keys */\n\n");
+        fprintf(f, "CFBundleName = \"BOINC\";\n");
+        fprintf(f, "CFBundleShortVersionString = \"BOINC version %s\";\n", BOINC_VERSION_STRING);
+        fprintf(f, "CFBundleGetInfoString = \"BOINC version %s, Copyright 2005 University of California.\";\n", BOINC_VERSION_STRING);
+        retval = fclose(f);
+    }
+    else {
+        puts("Error updating version number in file InfoPlist.strings\n");
+        retval = -1;
+    }
+        
+    return retval;
+}
+
+
+int FixInfoPlistFile(char* myPath) {
+    int retval = 0;
+    FILE *fin = NULL, *fout = NULL;
+    char *c, a, buf[1024];
+    
+    if (IsFileCurrent(myPath))
+        return 0;
+
+    fin = fopen(myPath, "r");
+    if (fin == NULL)
+        goto bail;
+
+    fout = fopen("temp", "w");
+    if (fout == NULL) {
+        fclose(fin);
+        goto bail;
+    }
+
+    // Copy everything up to version number
+    for (;;) {
+        c = fgets(buf, sizeof(buf), fin);
+        if (c == NULL)
+            goto bail;   // EOF
+        c = strstr(buf, "CFBundleVersion</key>");
+        if (c)
+            break;  // Found "CFBundleVersion</key>"
+        fputs(buf, fout);
+    }
+        
+    c = strstr(buf, "<string>");
+    if (c == NULL) {
+        fputs(buf, fout);
+        c = fgets(buf, sizeof(buf), fin);
+        if (c == NULL)
+            goto bail;   // EOF
+        c = strstr(buf, "<string>");
+        if (c == NULL)
+            goto bail;   // "CFBundleVersion</key>" not followed by "<string>"
+    }
+    
+    a = *(c+8);
+    *(c+8) = '\0';                      // Put terminator after "<string>"
+    fputs(buf, fout);                   // Copy up to end of "<string>"
+    fputs(BOINC_VERSION_STRING, fout);  // Write the current version number
+    *(c+8) = a;                         // Undo terminator we inserted
+    c = strstr(buf, "</string>");       // Skip over old version number in input
+    fputs(c, fout);                     // Copy rest of input line
+
+    // Copy rest of file
+    for (;;) {
+        c = fgets(buf, sizeof(buf), fin);
+        if (c == NULL)
+            break;   // EOF
+        fputs(buf, fout);
+    }
+
+    fclose(fin);
+    fclose(fout);
+    
+    sprintf(buf, "mv -f temp %s", myPath);
+    retval = system(buf);
+    
+    return retval;
+
+bail:
+    if (fin)
+        fclose(fin);
+    if (fout)
+        fclose(fout);
+
+    printf("Error updating version number in file %s\n", myPath);
+    return -1;
+}
+
+
+int MakeInstallerInfoPlistFile() {
+    int retval = 0;
+    FILE *f;
+    char *myPath = "./Pkg-Info.plist";
+    
+    if (IsFileCurrent(myPath))
+        return 0;
+
+    f = fopen(myPath, "w");
+    if (f)
+    {
+        fprintf(f, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        fprintf(f, "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n");
+        fprintf(f, "<plist version=\"1.0\">\n<dict>\n");
+        fprintf(f, "\t<key>CFBundleGetInfoString</key>\n");
+        fprintf(f, "\t<string>BOINC Manager %s</string>\n", BOINC_VERSION_STRING);
+        fprintf(f, "\t<key>CFBundleIdentifier</key>\n\t<string>edu.berkeley.boinc</string>\n");
+        fprintf(f, "\t<key>CFBundleShortVersionString</key>\n");
+        fprintf(f, "\t<string>%s</string>\n", BOINC_VERSION_STRING);
+        fprintf(f, "\t<key>IFPkgFlagAllowBackRev</key>\n\t<integer>1</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagAuthorizationAction</key>\n\t<string>AdminAuthorization</string>\n");
+        fprintf(f, "\t<key>IFPkgFlagDefaultLocation</key>\n\t<string>/</string>\n");
+        fprintf(f, "\t<key>IFPkgFlagFollowLinks</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagInstallFat</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagInstalledSize</key>\n\t<integer>6680</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagIsRequired</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagOverwritePermissions</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagRelocatable</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagRestartAction</key>\n\t<string>NoRestart</string>\n");
+        fprintf(f, "\t<key>IFPkgFlagRootVolumeOnly</key>\n\t<integer>1</integer>\n");
+        fprintf(f, "\t<key>IFPkgFlagUpdateInstalledLanguages</key>\n\t<integer>0</integer>\n");
+        fprintf(f, "\t<key>IFPkgFormatVersion</key>\n\t<real>0.10000000149011612</real>\n");
+        fprintf(f, "</dict>\n</plist>\n");
+
+        retval = fclose(f);
+    }
+    else {
+        puts("Error creating file Pkg-Info.plist\n");
+        retval = -1;
+    }
+        
+    return retval;
+}
+
+
+const char *BOINC_RCSID_9263a2dc22="$Id$";
