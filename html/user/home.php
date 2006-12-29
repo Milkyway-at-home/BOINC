9bb28dfdff4c518084600aaa2f800376ea6e88d6
David Anderson <davea@ssl.berkeley.edu>
2006-12-29 19:01:03 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=11738

---
+++ html/user/home.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("../inc/util.inc");
+require_once("../inc/user.inc");
+require_once("../inc/db.inc");
+require_once("../inc/forum.inc");
+
+// show the home page of whoever's logged in
+
+db_init();
+$user = get_logged_in_user();
+$user = getForumPreferences($user);
+$user = get_other_projects($user);
+page_head("Your account");
+
+if (get_str("new_acct", true)) {
+    echo "
+        <p>
+        Welcome to ".PROJECT.".
+        View and edit your account preferences using the links below.
+    ";
+}
+if (get_str("via_web", true)) {
+    echo "
+        <p>
+        If you have not already done so,
+        <a href=http://boinc.berkeley.edu/download.php>download BOINC client software</a>.
+    ";
+}
+echo "<p>\n";
+
+show_user_page_private($user);
+show_other_projects($user, true);
+project_user_page_private($user);
+
+page_tail();
+
+?>
