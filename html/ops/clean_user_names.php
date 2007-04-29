4e6586b2deabe143146c64f1d5eb844fb8d8f8ea
Rytis Slatkevičius <rytis.s@gmail.com>
2007-04-29 14:45:44 +0000

    Fix for ticket #125 - problems with websites using PHP setting short_open_tag (additional files)
    svn path=/trunk/boinc/; revision=12500

---
+++ html/ops/clean_user_names.php
@@ -0,0 +1,30 @@
+<?php
+require_once("../inc/db.inc");
+
+set_time_limit(0);
+
+db_init();
+
+function clean_user($user) {
+    if ($user->name != strip_tags($user->name)) {
+        $x = strip_tags($user->name);
+        echo "ID: $user->id
+name: $user->name
+stripped name: $x
+email: $user->email_addr
+-----
+";
+        $x = boinc_real_escape_string($x);
+        $x = trim($x);
+        $query = "update user set name='$x' where id=$user->id";
+        $retval = mysql_query($query);
+        echo $query;
+    }
+}
+
+$result = mysql_query("select id, name, email_addr from user");
+while ($user = mysql_fetch_object($result)) {
+    clean_user($user);
+}
+
+?>
