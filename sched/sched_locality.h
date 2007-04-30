94c9aea2f4e3f613041100a52c814e4ed35a3025
David Anderson <davea@ssl.berkeley.edu>
2007-04-30 21:19:24 +0000

    - scheduler: added (correct this time!) support for         <alt_platform> tags in scheduler requests.     - file_deleter: add check for -dont_delete_batches
        sched/
            file_deleter.C
            handle_request.C
            sched_array.C,h
            sched_locality.C,h
            sched_resend.C,h
            sched_send.C,h
            server_types.h
    
    svn path=/trunk/boinc/; revision=12512

---
+++ sched/sched_locality.h
@@ -0,0 +1,28 @@
+// Berkeley Open Infrastructure for Network Computing
+// http://boinc.berkeley.edu
+// Copyright (C) 2005 University of California
+//
+// This is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation;
+// either version 2.1 of the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// To view the GNU Lesser General Public License visit
+// http://www.gnu.org/copyleft/lesser.html
+// or write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+extern void send_work_locality(
+    SCHEDULER_REQUEST&, SCHEDULER_REPLY&, PLATFORM_LIST&, SCHED_SHMEM&
+);
+
+extern int decrement_disk_space_locality(
+    WORKUNIT& wu, SCHEDULER_REQUEST& request, SCHEDULER_REPLY& reply
+);
+
+extern int delete_file_from_host(SCHEDULER_REQUEST&, SCHEDULER_REPLY&);
---
+++ sched/sched_resend.h
@@ -0,0 +1,4 @@
+extern bool resend_lost_work(
+    SCHEDULER_REQUEST&, SCHEDULER_REPLY&, PLATFORM_LIST&, SCHED_SHMEM&
+);
+
