d5f05c609b02737340682bbfca7ee86c918558c7
Rytis Slatkevičius <rytis.s@gmail.com>
2007-05-18 15:02:02 +0000

    Encode UTF characters into HTML entities (from ChristianB, fix #57).
    NOTE: teams that have name display issues will have to edit their description once the projects update the code.
    
    svn path=/trunk/boinc/; revision=12691

---
+++ html/user/edit_user_info_action.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("../inc/db.inc");
+require_once("../inc/user.inc");
+require_once("../inc/util.inc");
+require_once("../inc/countries.inc");
+
+db_init();
+$user = get_logged_in_user();
+
+$name = boinc_htmlentities(process_user_text(post_str("user_name")));
+if ($name != strip_tags($name)) {
+    error_page("HTML tags not allowed in name");
+}
+if (strlen($name) == 0) {
+   error_page("You must supply a name for your account.");
+}
+$url = process_user_text(post_str("url", true));
+$url = strip_tags($url);
+$country = post_str("country");
+if (!is_valid_country($country)) {
+    error_page("bad country");
+}
+$postal_code = process_user_text(post_str("postal_code",true));
+$postal_code = strip_tags($postal_code);
+
+$result = mysql_query("update user set name='$name', url='$url', country='$country', postal_code='$postal_code' where id=$user->id");
+if ($result) {
+    Header("Location: home.php");
+} else {
+    error_page("Couldn't update user info.");
+}
+
+?>
