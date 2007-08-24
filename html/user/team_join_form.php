3542ebbf9350187a7f992b70a1e7cab18c12545f
Rytis Slatkevičius <rytis.s@gmail.com>
2007-08-24 15:41:01 +0000

    User web: fix XSS vulnerability in team join/quit/lookup code.
    svn path=/trunk/boinc/; revision=13450

---
+++ html/user/team_join_form.php
@@ -0,0 +1,30 @@
+<?php
+$cvs_version_tracker[]="\$Id$";  //Generated automatically - do not edit
+
+require_once("../inc/db.inc");
+require_once("../inc/util.inc");
+require_once("../inc/team.inc");
+
+db_init();
+$user = get_logged_in_user();
+$teamid = get_int("id");
+
+$team = lookup_team($teamid);
+$team_name = $team->name;
+page_head("Join $team_name");
+echo " <p><b>Please note:</b>
+    <ul>
+    <li> Joining a team gives its founder access to your email address.
+    <li> Joining a team does not affect your account's credit.
+    </ul>
+    <hr>
+    <form method=\"post\" action=\"team_join_action.php\">";
+echo form_tokens($user->authenticator);
+echo "
+    <input type=\"hidden\" name=\"teamid\" value=\"$teamid\">
+    <input type=\"submit\" value=\"Join team\">
+    </form>
+";
+page_tail();
+
+?>
---
+++ html/user/team_quit_form.php
@@ -0,0 +1,34 @@
+<?php
+$cvs_version_tracker[]="\$Id$";  //Generated automatically - do not edit
+
+require_once("../inc/db.inc");
+require_once("../inc/util.inc");
+require_once("../inc/team.inc");
+
+db_init();
+$user = get_logged_in_user(true);
+
+$team = lookup_team($user->teamid);
+if (!$team) {
+    error_page("No such team");
+}
+
+page_head("Quit $team->name");
+echo "
+    <b>Please note before quitting a team:</b>
+    <ul>
+    <li>If you quit a team, you may rejoin later,
+    or join any other team you desire
+    <li>Quitting a team does not affect your personal credit
+    statistics in any way.
+    </ul>
+    </p>
+    <form method=\"post\" action=\"team_quit_action.php\">";
+echo form_tokens($user->authenticator);
+echo "<input type=\"hidden\" name=\"id\" value=\"$team->id\">
+    <input type=\"submit\" value=\"Quit Team\">
+    </form>
+";
+page_tail();
+
+?>
