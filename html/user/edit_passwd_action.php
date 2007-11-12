2421b5436d3f954648397e9b702b92cd79b0731f
David Anderson <davea@ssl.berkeley.edu>
2007-11-12 22:28:17 +0000

    - user web: update other code to use new-style DB interfaces
    svn path=/trunk/boinc/; revision=14164

---
+++ html/user/edit_passwd_action.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once("../inc/boinc_db.inc");
+require_once("../inc/util.inc");
+require_once("../inc/user.inc");
+
+$auth = process_user_text(post_str("auth", true));
+$email_addr = strtolower(process_user_text(post_str("email_addr", true)));
+
+// Note: don't call process_user_text() on passwords.
+// This is not needed, and will break passwords containing punctuation
+
+$old_passwd = stripslashes(post_str("old_passwd", true));
+$passwd = stripslashes(post_str("passwd"));
+$passwd2 = stripslashes(post_str("passwd2"));
+
+if ($passwd != $passwd2) {
+    error_page("New passwords are different");
+}
+
+$config = get_config();
+$min_passwd_length = parse_config($config, "<min_passwd_length>");
+if (!$min_passwd_length) $min_passwd_length = 6;
+
+if (!is_ascii($passwd)) {
+    error_page("Passwords may only include ASCII characters.");
+}
+
+if (strlen($passwd)<$min_passwd_length) {
+    error_page(
+        "New password is too short:
+        minimum password length is $min_passwd_length characters."
+    );
+}
+if ($auth) {
+    $user = lookup_user_auth($auth);
+    if (!$user) {
+        error_page("Invalid account key");
+    }
+} else {
+    $user = lookup_user_email_addr($email_addr);
+    if (!$user) {
+        error_page("No account with that email address was found");
+    }
+    $passwd_hash = md5($old_passwd.$email_addr);
+    if ($user->passwd_hash != $passwd_hash) {
+        error_page("Invalid password");
+    }
+}
+
+page_head("Change password");
+$passwd_hash = md5($passwd.$user->email_addr);
+$result = $user->update("passwd_hash='$passwd_hash'");
+if ($result) {
+    echo "Your password has been changed.";
+} else {
+    echo "
+        We can't update your password due to a database problem.
+        Please try again later.
+    ";
+}
+
+page_tail();
+?>
---
+++ html/user/profile_rate.php
@@ -0,0 +1,31 @@
+<?php
+ 
+require_once("../inc/boinc_db.inc");
+
+$userid = $_GET['userid'];
+$vote = $_GET['vote'];
+
+if ($vote != "recommend" && $vote != "reject") {
+    echo "Invalid vote type.<br>";
+    exit();
+}
+
+BoincProfile::update_aux("$vote=$vote+1 WHERE userid = $userid");
+
+page_head("Vote Recorded");
+
+start_table_noborder();
+
+row1("Thank you");
+
+if ($vote == "recommend") {
+    rowify("Your recommendation has been recorded.");
+} else {
+    rowify("Your vote to reject has been recorded.");
+}
+end_table();
+echo "<br><a href=\"view_profile.php?userid=", $userid ,"\">Return to profile.</a>";
+
+page_tail();
+
+?>
