c83b2f5534e8082fde7f5ef415683c5bdaa83d0f
David Anderson <davea@ssl.berkeley.edu>
2007-11-20 19:23:26 +0000

    - user web: clean up funky search result formatting.     For thread titles, show links to forum/category also     For posts, show complete post, with search terms highlighted. - user web: in display of posts for a user,     show links to thread and forum also - user web: posts/threads can become "orphaned" if their forum     disappears, e.g. team message board is deleted.     Handle this gracefully in search results and user post list - user web: where HTML tags are allowed, say so - user web: fix bug where wrap_postcounts weren't being shown in form - DB: use "tinyint" instead of "tinyint(1) unsigned" etc.
    svn path=/trunk/boinc/; revision=14267

---
+++ html/user/html.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../inc/util.inc");
+
+page_head("Allowed HTML tags");
+
+echo "
+The following HTML tags are allowed in profiles, team descriptions,
+signatures, etc.:
+<ul>
+<li> &lt;b> or &lt;strong> (bold)
+<li> &lt;i> or &lt;em> (italics)
+<li> &lt;a> (hyperlink)
+<li> &lt;p> (paragraph)
+<li> &lt;br> (break)
+<li> &lt;pre> (preformatted)
+<li> &lt;img> (image; height cannot exceed 450 pixels.
+Please do not link to images without
+permission of the web site where the image is hosted.)
+</ul>
+You can also use ampersand notation for special characters.
+";
+
+page_tail();
+?>
