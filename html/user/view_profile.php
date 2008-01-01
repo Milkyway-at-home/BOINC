08e3daf57ba979a223a608eebc23b646495afcdf
David Anderson <davea@ssl.berkeley.edu>
2008-01-01 22:29:10 +0000

    - web: My Account page, other user account page, and profile pages     now have community info in a 2nd column on the right.     This accomplishes two things:     1) it puts community info in more places;     2) it provides a reasonable way to format long lists of friends;         they just continue down the right column.
    svn path=/trunk/boinc/; revision=14452

---
+++ html/user/view_profile.php
@@ -0,0 +1,37 @@
+<?php
+ 
+require_once("../inc/profile.inc");
+
+$userid = get_int('userid');
+$user = BoincUser::lookup_id($userid);
+if (!$user) {
+    error_page("No such user");
+}
+ 
+$logged_in_user = get_logged_in_user(false);
+$caching = false;
+if (!$logged_in_user || ($userid != $logged_in_user->id)) {
+    $caching = true;
+    $cache_args = "userid=$userid";
+    start_cache(USER_PROFILE_TTL,$cache_args);
+}
+page_head("Profile: $user->name");
+start_table();
+echo "<tr><td valign=top>";
+start_table();
+show_profile($user, $logged_in_user);
+end_table();
+echo "</td><td valign=top>";
+start_table();
+row2("Account data", "<a href=show_user.php?userid=$userid>View</a>");
+community_links($user);
+end_table();
+echo "</td></tr></table>";
+
+page_tail();
+
+if ($caching) {
+    end_cache(USER_PROFILE_TTL, $cache_args);
+}
+
+?>
