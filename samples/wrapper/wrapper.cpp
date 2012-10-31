Milkyway-at-home/BOINC

commit 9223fd22e3b6b848cc9e5e151d958542da8fd49d

- wrapper: compile fix for new gcc

samples/wrapper/wrapper.cpp
@@ -38,6 +38,8 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <unistd.h>
 #endif
 

checkin_notes
@@ -6468,3 +6468,9 @@ David  29 Oct 2012
         gui_rpc_client.h
         gui_rpc_client_ops.cpp
         gui_rpc_client_print.cpp
+
+David  31 Oct 2012
+    - wrapper: compile fix for new gcc
+
+    samples/wrapper/
+        wrapper.cpp

commit 2d0150c6872bd3f0489a90a71e2728c3a8e792fb

- wrapper: fix for likely compile problems on non-linux or older gcc caused by    commit 9223fd2.  The macros necessary to fix it already existed in config.h

samples/wrapper/wrapper.cpp
@@ -28,6 +28,9 @@
 // See http://boinc.berkeley.edu/trac/wiki/WrapperApp for details
 // Contributor: Andrew J. Younge (ajy4490@umiacs.umd.edu)
 
+#ifndef _WIN32
+#include "config.h"
+#endif
 #include <stdio.h>
 #include <vector>
 #include <string>
@@ -35,11 +38,17 @@
 #include "boinc_win.h"
 #include "win_util.h"
 #else
+#ifdef HAVE_SYS_WAIT_H
 #include <sys/wait.h>
+#endif
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
+#endif
+#ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
+#endif
 #include <unistd.h>
 #endif
 